ba5dc7b4f82db2378958080d7af09975d68dab28
akulbaeva <aiymbubu.kulbaeva@iaau.edu.kg>
2019-12-17 15:47:45 +0600

    Add some images

--- src/sample/LogIn.java
+++ src/sample/LogIn.java
@@ -1,5 +1,57 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
 public class LogIn {
+
+    @FXML
+    private TextField tfUsername;
+
+    @FXML
+    private PasswordField pfPassword;
+
+    @FXML
+    private Button btnLogIn;
+
+    @FXML
+    private Button btnSignIn;
+
+    @FXML
+    void btnLogInClick(ActionEvent event) {
+        Alert message = null;
+
+        if(tfUsername.getText().equals("com-18") && pfPassword.getText().equals("com")){
+            try{
+                Node node = (Node) event.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+
+                stage.close();
+
+                stage.setOpacity(0.0);
+
+                Scene scene = new Scene(FXMLLoader.load(getClass().getResource("FXMLDocument.fxml")));
+                stage.setScene(scene);;
+                stage.setMaximized(true);
+                stage.setResizable(true);
+                stage.show();
+
+                stage.setOpacity(0.0);
+            } catch (IOException e) {
+                System.out.println(e.toString());
+            }
+        } else {
+            message = new Alert(Alert.AlertType.NONE, "Log in failed" , ButtonType.OK);
+            message.showAndWait();
+        }
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,20 +4,35 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    Stage window;
+    Scene scene1, scene2;
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("LogIn.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+        stage.setTitle("Moon Entertainment Cinemas - Seat Reservation System");
+
+        try {
+            stage.getIcons().add(new Image("Icon/icon.png"));
+        }
+        catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
